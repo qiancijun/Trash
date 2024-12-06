29a4c0ae959227a28eb32c4041e4d5215dde5a83
Cheryl <769303522@qq.com>
2024-12-06 22:40:29 +0800

    2024/12/6

--- rpcDemo/codec/gob.go
+++ rpcDemo/codec/gob.go
@@ -4,14 +4,14 @@ import (
 	"bufio"
 	"encoding/gob"
 	"io"
+	"log"
 )
-
 
 /**
 * conn 是由构建函数传入，通常是通过 TCP 或者 Unix 建立 socket 时得到的链接实例
 * dec 和 enc 对应 gob 的 Decoder 和 Encoder
 * buf 是为了防止阻塞而创建的带缓冲的 Writer，一般这么做能提升性能。
-*/
+ */
 type GobCodec struct {
 	conn io.ReadWriteCloser
 	buf  *bufio.Writer
@@ -20,3 +20,44 @@ type GobCodec struct {
 }
 
 var _ Codec = (*GobCodec)(nil)
+
+func NewGobCodec(conn io.ReadWriteCloser) Codec {
+	buf := bufio.NewWriter(conn)
+	return &GobCodec{
+		conn: conn,
+		buf:  buf,
+		enc:  gob.NewEncoder(buf),
+		dec:  gob.NewDecoder(conn),
+	}
+}
+
+func (c *GobCodec) ReadHeader(h *Header) error {
+	return nil
+}
+
+func (c *GobCodec) ReadBody(body interface{}) error {
+	return nil
+}
+
+func (c *GobCodec) Write(h *Header, body interface{}) (err error) {
+	defer func() {
+		_ = c.buf.Flush()
+		if err != nil {
+			_ = c.Close()
+		}
+	}()
+
+	if err = c.enc.Encode(h); err != nil {
+		log.Println("rpc codec: gob error encoding header:", err)
+		return
+	}
+	if err = c.enc.Encode(body); err != nil {
+		log.Println("rpc codec: gob error encoding body:", err)
+		return
+	}
+	return nil
+}
+
+func (c *GobCodec) Close() error {
+	return c.conn.Close()
+}
---
+++ rpcDemo/server.go
@@ -0,0 +1,15 @@
+package rpcDemo
+
+import "github.com/qiancijun/rpcDemo/codec"
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int
+	CodecType   codec.Type
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
